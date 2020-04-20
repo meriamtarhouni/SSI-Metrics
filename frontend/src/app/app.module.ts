369febfe0f4fee30c91a7533ee3d0b2a7c722afc
zeineb chabchoub <zeinebchabchoub97@gmail.com>
2020-04-20 09:14:44 +0100

    Frontend CRUD collaborator

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
 import { LoginPageCollaboratorComponent } from './pages/login-page-collaborator/login-page-collaborator.component';
 import { SignupPageCollaboratorComponent } from './pages/signup-page-collaborator/signup-page-collaborator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
