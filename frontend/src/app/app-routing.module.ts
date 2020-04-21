dfe23b5feb696e794a6d149011c2e5359ee1981a
zeineb chabchoub <zeinebchabchoub97@gmail.com>
2020-04-21 07:37:31 +0100

    Routing System (Login & Sign up Collaborator)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginPageCollaboratorComponent} from '../app/pages/login-page-collaborator/login-page-collaborator.component';
+import {SignupPageCollaboratorComponent} from '../app/pages/signup-page-collaborator/signup-page-collaborator.component';
+import { from } from 'rxjs';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-collab', component: LoginPageCollaboratorComponent },
+  { path: 'signup-collab', component: SignupPageCollaboratorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
