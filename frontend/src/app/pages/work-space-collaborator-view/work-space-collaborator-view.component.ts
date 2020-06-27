175fdc7d434a118549bcb5dc92e59b961c8d6c76
zeineb chabchoub <zeinebchabchoub97@gmail.com>
2020-06-27 09:59:51 +0100

    warning PDCA

--- frontend/src/app/pages/work-space-collaborator-view/work-space-collaborator-view.component.ts
+++ frontend/src/app/pages/work-space-collaborator-view/work-space-collaborator-view.component.ts
@@ -14,11 +14,37 @@ export class WorkSpaceCollaboratorViewComponent implements OnInit {
 
   constructor(private phaseService: PhaseService, private authCollaboratorService: AuthCollaboratorService) { }
 
-  showNotification(from, align, type) {
+  showNotificationPlan(from, align, type) {
 
     $.notify({
       icon: "notifications",
-      message: "<br/>PDCA est une méthode de gestion itérative.Vous ne pouvez entamer une phase que lorsque la phase précedente est mise en oeuvre<br/>"
+      message: "<br/>PDCA est une méthode de gestion itérative.Vous ne pouvez entamer une phase que lorsque la phase PLAN est mise en oeuvre<br/>"
+
+    }, {
+      type: type,
+      timer: 4000,
+      placement: {
+        from: from,
+        align: align
+      },
+      template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+        '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+        '<i class="material-icons" data-notify="icon">error_outline</i> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<div class="progress" data-notify="progressbar">' +
+        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+        '</div>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+        '</div>'
+    });
+
+  }
+  showNotificationDo(from, align, type) {
+
+    $.notify({
+      icon: "notifications",
+      message: "<br/>PDCA est une méthode de gestion itérative.Vous ne pouvez entamer une phase que lorsque la phase DO est mise en oeuvre<br/>"
 
     }, {
       type: type,
@@ -41,6 +67,32 @@ export class WorkSpaceCollaboratorViewComponent implements OnInit {
 
   }
 
+  showNotificationCheck(from, align, type) {
+
+    $.notify({
+      icon: "notifications",
+      message: "<br/>PDCA est une méthode de gestion itérative.Vous ne pouvez entamer une phase que lorsque la phase CHECK est mise en oeuvre<br/>"
+
+    }, {
+      type: type,
+      timer: 4000,
+      placement: {
+        from: from,
+        align: align
+      },
+      template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+        '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+        '<i class="material-icons" data-notify="icon">error_outline</i> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<div class="progress" data-notify="progressbar">' +
+        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+        '</div>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+        '</div>'
+    });
+
+  }
 
   ngOnInit(): void {
 
